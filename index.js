94a279489927ab07f68166b9d1ee1b99086d374c
Wyatt Ades <wyattades@gmail.com>
2016-04-11 12:11:28 -0700

    Add database

--- index.js
+++ index.js
@@ -17,4 +17,18 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.postgres://lexgdugeqzutzz:sgfUU6yLh2u9sClAL4lx5h2tmV@ec2-54-235-85-65.compute-1.amazonaws.com:5432/degs89re0755ap, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
 
