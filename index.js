725dd4b97594333a2a201b0c83ae31253cf2dea8
wyattades <wyattades@gmail.com>
2016-04-15 22:17:50 -0400

    moved package.json line

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var uuid = require('node-uuid');
-var mongoose = require('mongoose');
+// var uuid = require('node-uuid');
+// var mongoose = require('mongoose');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,33 +10,33 @@ app.get('/', function(req, res) {
 });
 
 // Add validation that the logged in user can view the content
-// app.use(function() {
+app.use(function() {
    
-// });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
 
-var Schema = mongoose.schema;
-mongoose.connect('mongodb://heroku_nhh85smw:qied5ud6mac0hfvatgkbpk4l2m@ds011241.mlab.com:11241/heroku_nhh85smw');
+// var Schema = mongoose.schema;
+// mongoose.connect('mongodb://heroku_nhh85smw:qied5ud6mac0hfvatgkbpk4l2m@ds011241.mlab.com:11241/heroku_nhh85smw');
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   // we're connected!
+// });
 
-var userSchema = Schema({
-  name:  String,
-  id: String
-});
-var User = mongoose.model('User', userSchema);
+// var userSchema = Schema({
+//   name:  String,
+//   id: String
+// });
+// var User = mongoose.model('User', userSchema);
 
-//TEMP:
+// //TEMP:
 
-var testUser = new User({ name: 'FooBarMcGee', id: 'Poo' });
-console.log(testUser.name);
+// var testUser = new User({ name: 'FooBarMcGee', id: 'Poop' });
+// console.log(testUser.name);
 
 
