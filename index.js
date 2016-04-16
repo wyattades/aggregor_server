2fc8661da6d03011232813af6a2ee0dfd5c95e27
wyattades <wyattades@gmail.com>
2016-04-15 22:56:48 -0400

    updated routes

--- index.js
+++ index.js
@@ -3,16 +3,11 @@ var app = express();
 // var uuid = require('node-uuid');
 // var mongoose = require('mongoose');
 
+var apiRouter = require('./api_routes.js').apiRouter;
+
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(req, res) {
-  res.status(200).json({text: 'hello, world!'});
-});
-
-// Add validation that the logged in user can view the content
-app.use(function() {
-   
-});
+app.use('/api/', apiRouter);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
