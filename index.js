fefd5a874840e27bfe2ce8ab8a9e3019b3128375
wyattades <wyattades@gmail.com>
2016-04-15 21:53:54 -0400

    remove .ejs dependencies

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
