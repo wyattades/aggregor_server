a8ccd53ba643fbf1479345f2ce7bcb1015eacf1c
Spenser Williams <spenser@w2cs.com>
2016-04-16 09:50:18 -0700

    re-add dotenv

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 
 var express = require('express');
 var app = express();
 
 var mongoose = require('mongoose');
+console.log('Connecting to db: ' + process.env.DB_HOST);
 mongoose.connect(process.env.DB_HOST);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
