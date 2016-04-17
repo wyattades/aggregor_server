780aea66b5142e319e758c906e0b1dbc5ad31b92
wyattades <wyattades@gmail.com>
2016-04-16 17:13:06 -0700

    Grunt infastructure in place

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,10 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       all: ['Gruntfile.js', 'index.js']
+    },
+    nodeunit: {
+      all: ['test/**/*.js']
     }
-    //,
-    //nodeunit: {
-    //  all: ['test/**/*.js']
-    //}
   });
  
   grunt.loadNpmTasks('grunt-contrib-jshint');
