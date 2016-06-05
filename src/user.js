d22f85b39418131d091eadb5936d438223c4d9f9
wyattades <wyattades@gmail.com>
2016-06-05 15:28:03 -0700

    Changed github references to GatorCoding

--- src/user.js
+++ src/user.js
@@ -1,8 +1,8 @@
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 
-var UserSchema = new mongoose.Schema({
-  name: String,
-  id: String  
-});
+//var UserSchema = new mongoose.Schema({
+//  name: String,
+//  id: String  
+//});
 
-module.exports = mongoose.model('User', UserSchema);
+//module.exports = mongoose.model('User', UserSchema);
