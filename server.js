db3685081418480dc07fcb9e8198130d3948d1f6
Spenser Williams <spenser@w2cs.com>
2016-12-09 22:26:14 -0800

    test fix cors

--- server.js
+++ server.js
@@ -165,9 +165,20 @@ function run(port) {
     const [route, match] = matchUrl(req);
     let authInfo = {};
 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
     if(!route) {
       respond(404, 'Not Found', 'No route ' + req.url);
       return;
+    } else {
+      if(req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        res.writeHead(200);
+        res.end();
+
+        return;
+      }
     }
 
     if(route.authenticate) {
