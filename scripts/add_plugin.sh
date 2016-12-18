18b3a12440dcafa39dc660612576f932996d3819
Spenser Williams <spenser@w2cs.com>
2016-12-18 15:17:34 -0800

    fix protocol raw feeds

--- scripts/add_plugin.sh
+++ scripts/add_plugin.sh
@@ -2,5 +2,5 @@
 curl -X POST \
      -H "Content-Type: application/json" \
      -H "X-Aggregor-Token: $1" \
-     -d '{"type": "raw", "data": {"url": "https://www.reddit.com/r/Overwatch/"}}' \
+     -d '{"type": "raw", "data": {"url": "www.reddit.com/r/Overwatch/"}}' \
      localhost:3000/user/spenserw/feed/news
--- src/plugins/raw.js
+++ src/plugins/raw.js
@@ -1,13 +1,18 @@
 const request = require('request'),
-      through2 = require('through2');
+      through2 = require('through2'),
+      url = require('url');
 
 exports.type = 'raw';
 
 exports.request = function(data) {
-  const pt = through2(),
-        url = data.url;
+  const pt = through2();
+  var reqUrl = data.url;
 
-  const req = request(url);
+  const protocol = url.parse(reqUrl).protocol;
+  if(!protocol)
+    reqUrl = 'http://' + reqUrl;
+
+  const req = request(reqUrl);
   req.on('error', (err) => {
     pt.end();
   });
