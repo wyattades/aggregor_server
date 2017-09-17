9a2869ed34631d1c177f55e4e180764fa6720038
Wyatt <wyattades@gmail.com>
2017-09-17 15:36:40 -0700

    updated node, added migrate_up compatability for windows

--- scripts/migrate_up.js
+++ scripts/migrate_up.js
@@ -2,7 +2,11 @@ const exec = require('child_process').exec;
 
 const DB_URI = require('../config').DB_URI;
 
-exec(`export DATABASE_URL=${DB_URI} && ./node_modules/.bin/pg-migrate up`, (err, stdout, stderr) => {
+const cmd = process.platform === 'win32' ? 
+`set "DATABASE_URL=${DB_URI}" && node_modules\\.bin\\pg-migrate.cmd up` :
+`export DATABASE_URL=${DB_URI} && ./node_modules/.bin/pg-migrate up`;
+
+exec(cmd, (err, stdout, stderr) => {
   if(err) {
     console.log("Failed to run migrations!");
     console.log(stderr);
